8cdc7557f01a57f76668df353a398111685ce6f9
veednak <44851565+veednak@users.noreply.github.com>
2022-03-22 01:50:53 +0300

    Fix

--- todo/main.js
+++ todo/main.js
@@ -1,13 +1,13 @@
-let addingMessage = document.querySelector('.text-todo');
-let addingMessageSearch = document.querySelector('.text-search');
-// let addButton = document.querySelector('.button-plus');
-let searchInput = document.querySelector('#search');
-let toDo = document.querySelector('.todo');
-let toDoViewing = document.querySelector('.viewing');
-let done = document.querySelector('.done');
-let all = document.querySelector('.all');
-let clear = document.querySelector('.clear-all');
-let adding = document.querySelector('#adding');
+const newTaskMessage = document.querySelector(".text-todo");
+const addingButton = document.getElementById("addTask");
+const selectAll = document.querySelector(".select-all");
+const searchInput = document.querySelector("#search");
+const currentTasks = document.querySelector(".todo");
+const toDoViewing = document.querySelector("#viewing");
+const completedTasks = document.querySelector("#done");
+const fullTasksList = document.querySelector("#all");
+const clear = document.querySelector(".clear-all");
+const adding = document.querySelector("#adding");
 
 let toDoList = [];
 
@@ -15,107 +15,111 @@ function byField(field) {
   return (a, b) => (a[field] > b[field] ? 1 : -1);
 }
 
-if (localStorage.getItem('todo')) {
-  toDoList = JSON.parse(localStorage.getItem('todo'));
+if (localStorage.getItem("todo")) {
+  toDoList = JSON.parse(localStorage.getItem("todo"));
   displayMessages();
 }
 
-function clearButts() {
-  const butts = Array.from(document.querySelector('.line').children);
-  butts.forEach((item) => {
-    item.classList.remove('active');
+function resetButtonsActivity() {
+  const allButtons = Array.from(document.querySelector(".butts").children);
+  allButtons.forEach((item) => {
+    item.classList.remove("active");
   });
 }
 
-clear.addEventListener('click', function () {
-  toDoList=[];
-  localStorage.setItem('todo', JSON.stringify(toDoList));
+selectAll.addEventListener("click", function () {
+  toDoList.forEach((item) => {
+    item.checked = true;
+  });
   displayMessages();
+  localStorage.setItem("todo", JSON.stringify(toDoList));
 });
-toDoViewing.addEventListener('click', function () {
-  const items = Array.from(document.querySelectorAll('.item'));
-  clearButts();
-  this.classList.add('active');
+
+toDoViewing.addEventListener("click", function () {
+  const items = Array.from(document.querySelectorAll(".item"));
+  resetButtonsActivity();
+  this.classList.add("active");
   items.forEach((item) => {
-    if (!item.classList.contains('doneItem')) {
-      item.classList.remove('hidden');
+    if (!item.classList.contains("doneItem")) {
+      item.classList.remove("hidden");
     } else {
-      item.classList.add('hidden');
+      item.classList.add("hidden");
     }
   });
 });
 
-done.addEventListener('click', function () {
-  const items = Array.from(document.querySelectorAll('.item'));
-  clearButts();
-  this.classList.add('active');
+completedTasks.addEventListener("click", function () {
+  const items = Array.from(document.querySelectorAll(".item"));
+  resetButtonsActivity();
+  this.classList.add("active");
   items.forEach((item) => {
-    if (item.classList.contains('doneItem')) {
-      item.classList.remove('hidden');
+    if (item.classList.contains("doneItem")) {
+      item.classList.remove("hidden");
     } else {
-      item.classList.add('hidden');
+      item.classList.add("hidden");
     }
   });
 });
 
-
-all.addEventListener('click', function () {
-  clearButts();
-  this.classList.add('active');
+fullTasksList.addEventListener("click", function () {
+  resetButtonsActivity();
+  this.classList.add("active");
   displayMessages();
 });
 
-// addButton.addEventListener('click', function () {
-
-//   addingToDo();
-// });
-
-searchInput.addEventListener('change', function (event) {
+searchInput.addEventListener("input", function (event) {
   const serchStr = this.value;
   let searchList = [];
-  const items = Array.from(document.querySelectorAll('.item'));
-  items.forEach((item,i) => {
-    let label=  toDoList[i].toDo;
+  const items = Array.from(document.querySelectorAll(".item"));
+  items.forEach((item, i) => {
+    let label = toDoList[i].toDo;
     if (label.includes(serchStr)) {
-      item.classList.remove('hidden');
+      item.classList.remove("hidden");
     } else {
-       item.classList.add('hidden');
+      item.classList.add("hidden");
     }
   });
-  searchInput.value = '';
 });
 
-adding.addEventListener('change', function (event) {
+adding.addEventListener("change", function (event) {
   let newToDo = {
     id: Math.random() * Date.now(),
-    toDo: addingMessage.value,
+    toDo: newTaskMessage.value,
     checked: false,
     important: false,
   };
   toDoList.push(newToDo);
-  adding.value = '';
+  adding.value = "";
   displayMessages();
-  localStorage.setItem('todo', JSON.stringify(toDoList));
+  localStorage.setItem("todo", JSON.stringify(toDoList));
 });
 
+adding.addEventListener("focus", function (event) {
+  addingButton.classList.remove("hidden");
+});
+
+adding.addEventListener("blur", function (event) {
+  addingButton.classList.add("hidden");
+});
 
 function addClick() {
-  let displayMessage = '';
+  addingButton.classList.add("hidden");
+  let displayMessage = "";
   toDoList.forEach(function (item, i) {
     displayMessage += `
             <li class="item ${
-              item.checked ? 'doneItem' : ''
+              item.checked ? "doneItem" : ""
             }" draggable = "true" id = "${item.id}">
-            <img class="button-drag-image" src="img/Drag_vertical.svg">
+            <img class="button-drag-image" src="img/DragVertical.svg">
                 <input class="todoli" type='checkbox' data-id="${
                   item.id
-                }" id='item_${i}'  ${item.checked ? 'checked' : ''}>
+                }" id='item_${i}'  ${item.checked ? "checked" : ""}>
               
                 <label  class="label-text" id = "${
                   item.id
                 }" for='item_${i}' text = "${item.toDo}">
                 <input id="editing" class="text-list ${
-                  item.checked ? 'striked' : ''
+                  item.checked ? "striked" : ""
                 }" type="text-list" text = "${item.toDo}"  placeholder="${
       item.toDo
     }" readonly >
@@ -125,47 +129,49 @@ function addClick() {
       <img class="button-trash-image" src="img/Trash.svg">
       </button>
     <button class="button-editing"  id = "${item.id}">
-      <img class="button-editing-image" src="${item.checked? '': 'img/Pencil.svg'}">
+      <img class="button-editing-image" src="${
+        item.checked ? "" : "img/Pencil.svg"
+      }">
     </button>
             </li>
         `;
   });
-  toDo.innerHTML = displayMessage;
+  currentTasks.innerHTML = displayMessage;
   let trash = Array.from(document.querySelectorAll(`.button-trash`));
   trash.forEach((item, i) => {
-    item.addEventListener('click', function (event) {
-        if (item.id == toDoList[i].id) {
+    item.addEventListener("click", function (event) {
+      if (item.id == toDoList[i].id) {
         toDoList.splice(i, 1);
-        localStorage.setItem('todo', JSON.stringify(toDoList));
+        localStorage.setItem("todo", JSON.stringify(toDoList));
       }
       displayMessages();
     });
   });
 
-  let labelText = Array.from(document.querySelectorAll(`.text-list`));
-  let editing = Array.from(document.querySelectorAll(`.button-editing`));
-  let inputText = Array.from(document.querySelectorAll(`#editing`));
-  let editingImage = Array.from(document.querySelectorAll(`.button-editing-image`));
+  const labelText = Array.from(document.querySelectorAll(`.text-list`));
+  const editing = Array.from(document.querySelectorAll(`.button-editing`));
+  const inputText = Array.from(document.querySelectorAll(`#editing`));
+  const editingImage = Array.from(
+    document.querySelectorAll(`.button-editing-image`)
+  );
 
   editing.forEach((item, i) => {
-    item.addEventListener('click', function (event) {
-      labelText[i].removeAttribute('readonly');
-      console.log(editingImage[i]);
-      editingImage[i].src = './img/Done.svg'
+    item.addEventListener("click", function (event) {
+      labelText[i].removeAttribute("readonly");
+      editingImage[i].src = "./img/Done.svg";
     });
   });
   inputText.forEach((item, i) => {
-    item.addEventListener('change', function (event) {
+    item.addEventListener("change", function (event) {
       toDoList[i].toDo = this.value;
-      console.log(toDoList[i].toDo);
-      localStorage.setItem('todo', JSON.stringify(toDoList));
+      localStorage.setItem("todo", JSON.stringify(toDoList));
       displayMessages();
     });
   });
 
   let elem = Array.from(document.querySelectorAll(`.todoli`));
   elem.forEach((item) => {
-    item.addEventListener('change', function (event) {
+    item.addEventListener("change", function (event) {
       changeStatus(event);
       displayMessages();
     });
@@ -182,31 +188,31 @@ function changeStatus(event) {
   for (let i = 0; i < toDoList.length; i++) {
     if (toDoList[i].id == idInput) {
       toDoList[i].checked = !toDoList[i].checked;
-      toDoList.sort(byField('checked'));
-      localStorage.setItem('todo', JSON.stringify(toDoList));
+      toDoList.sort(byField("checked"));
+      localStorage.setItem("todo", JSON.stringify(toDoList));
       break;
     }
   }
 }
 function handleDragStart(e) {
-  this.style.opacity = '0.4';
+  this.style.opacity = "0.4";
 }
 
 function handleDragEnd(e) {
-  this.style.opacity = '1';
+  this.style.opacity = "1";
 }
 
-let items = document.querySelectorAll('.wrapper .item');
+let items = document.querySelectorAll(".wrapper .item");
 items.forEach(function (item) {
-  item.addEventListener('dragstart', handleDragStart);
-  item.addEventListener('dragend', handleDragEnd);
+  item.addEventListener("dragstart", handleDragStart);
+  item.addEventListener("dragend", handleDragEnd);
 });
 
 function handleDragEnd(e) {
-  this.style.opacity = '1';
+  this.style.opacity = "1";
 
   items.forEach(function (item) {
-    item.classList.remove('over');
+    item.classList.remove("over");
   });
 }
 
@@ -219,20 +225,20 @@ function handleDragOver(e) {
 }
 
 function handleDragEnter(e) {
-  this.classList.add('over');
+  this.classList.add("over");
 }
 
 function handleDragLeave(e) {
-  this.classList.remove('over');
+  this.classList.remove("over");
 }
 
 function handleDragStart(e) {
-  this.style.opacity = '0.4';
+  this.style.opacity = "0.4";
 
   dragSrcEl = this;
 
-  e.dataTransfer.effectAllowed = 'move';
-  e.dataTransfer.setData('text/html', this.innerHTML);
+  e.dataTransfer.effectAllowed = "move";
+  e.dataTransfer.setData("text/html", this.innerHTML);
 }
 
 function handleDrop(e) {
@@ -246,7 +252,7 @@ function handleDrop(e) {
       toDoList[itemIndex2],
       toDoList[itemIndex1],
     ];
-    localStorage.setItem('todo', JSON.stringify(toDoList));
+    localStorage.setItem("todo", JSON.stringify(toDoList));
     displayMessages();
   }
 
@@ -254,13 +260,13 @@ function handleDrop(e) {
 }
 
 function dragAndDropItems() {
-  let items = document.querySelectorAll('.wrapper .item');
+  let items = document.querySelectorAll(".wrapper .item");
   items.forEach(function (item) {
-    item.addEventListener('dragstart', handleDragStart);
-    item.addEventListener('dragover', handleDragOver);
-    item.addEventListener('dragenter', handleDragEnter);
-    item.addEventListener('dragleave', handleDragLeave);
-    item.addEventListener('dragend', handleDragEnd);
-    item.addEventListener('drop', handleDrop);
+    item.addEventListener("dragstart", handleDragStart);
+    item.addEventListener("dragover", handleDragOver);
+    item.addEventListener("dragenter", handleDragEnter);
+    item.addEventListener("dragleave", handleDragLeave);
+    item.addEventListener("dragend", handleDragEnd);
+    item.addEventListener("drop", handleDrop);
   });
 }
