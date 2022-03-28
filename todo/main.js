a0f07519c6bcbc93aad610a3c13def7be12f267a
veednak <44851565+veednak@users.noreply.github.com>
2022-03-28 12:29:48 +0300

    fix

--- todo/main.js
+++ todo/main.js
@@ -2,12 +2,11 @@ const newTaskMessage = document.querySelector(".text-todo");
 const addingButton = document.getElementById("addTask");
 const selectAll = document.querySelector(".select-all");
 const searchInput = document.querySelector("#search");
-const currentTasks = document.querySelector(".todo");
-const toDoViewing = document.querySelector("#viewing");
-const completedTasks = document.querySelector("#done");
-const fullTasksList = document.querySelector("#all");
-const clear = document.querySelector(".clear-all");
-const adding = document.querySelector("#adding");
+const currentTasks = document.querySelector(".list-todo");
+const toDoViewing = document.querySelector("#button-viewing-content");
+const completedTasks = document.querySelector("#button-done-content");
+const fullTasksList = document.querySelector("#button-all-content");
+const addingNewTask = document.querySelector("#new-task-id");
 
 let toDoList = [];
 
@@ -15,13 +14,15 @@ function byField(field) {
   return (a, b) => (a[field] > b[field] ? 1 : -1);
 }
 
-if (localStorage.getItem("todo")) {
-  toDoList = JSON.parse(localStorage.getItem("todo"));
+if (localStorage.getItem("list-todo")) {
+  toDoList = JSON.parse(localStorage.getItem("list-todo"));
   displayMessages();
 }
 
+fullTasksList.classList.add("active");
 function resetButtonsActivity() {
   const allButtons = Array.from(document.querySelector(".butts").children);
+
   allButtons.forEach((item) => {
     item.classList.remove("active");
   });
@@ -32,13 +33,14 @@ selectAll.addEventListener("click", function () {
     item.checked = true;
   });
   displayMessages();
-  localStorage.setItem("todo", JSON.stringify(toDoList));
+  localStorage.setItem("list-todo", JSON.stringify(toDoList));
 });
 
 toDoViewing.addEventListener("click", function () {
   const items = Array.from(document.querySelectorAll(".item"));
   resetButtonsActivity();
   this.classList.add("active");
+
   items.forEach((item) => {
     if (!item.classList.contains("doneItem")) {
       item.classList.remove("hidden");
@@ -52,6 +54,7 @@ completedTasks.addEventListener("click", function () {
   const items = Array.from(document.querySelectorAll(".item"));
   resetButtonsActivity();
   this.classList.add("active");
+
   items.forEach((item) => {
     if (item.classList.contains("doneItem")) {
       item.classList.remove("hidden");
@@ -71,6 +74,7 @@ searchInput.addEventListener("input", function (event) {
   const serchStr = this.value;
   let searchList = [];
   const items = Array.from(document.querySelectorAll(".item"));
+
   items.forEach((item, i) => {
     let label = toDoList[i].toDo;
     if (label.includes(serchStr)) {
@@ -81,7 +85,7 @@ searchInput.addEventListener("input", function (event) {
   });
 });
 
-adding.addEventListener("change", function (event) {
+addingNewTask.addEventListener("change", function (event) {
   let newToDo = {
     id: Math.random() * Date.now(),
     toDo: newTaskMessage.value,
@@ -89,16 +93,16 @@ adding.addEventListener("change", function (event) {
     important: false,
   };
   toDoList.push(newToDo);
-  adding.value = "";
+  addingNewTask.value = "";
   displayMessages();
-  localStorage.setItem("todo", JSON.stringify(toDoList));
+  localStorage.setItem("list-todo", JSON.stringify(toDoList));
 });
 
-adding.addEventListener("focus", function (event) {
+addingNewTask.addEventListener("focus", function (event) {
   addingButton.classList.remove("hidden");
 });
 
-adding.addEventListener("blur", function (event) {
+addingNewTask.addEventListener("blur", function (event) {
   addingButton.classList.add("hidden");
 });
 
@@ -112,23 +116,23 @@ function addClick() {
             }" draggable = "true" id = "${item.id}">
             <img class="button-drag-image" src="img/DragVertical.svg">
                 <input class="todoli" type='checkbox' data-id="${
-                  item.id
+                  item.id / 2
                 }" id='item_${i}'  ${item.checked ? "checked" : ""}>
               
-                <label  class="label-text" id = "${
-                  item.id
+                <label  class="list-item-text" id = "${
+                  item.id / 4
                 }" for='item_${i}' text = "${item.toDo}">
-                <input id="editing" class="text-list ${
+                <input id="editing-input" class="text-list ${
                   item.checked ? "striked" : ""
                 }" type="text-list" text = "${item.toDo}"  placeholder="${
       item.toDo
     }" readonly >
                       </label>
 
-    <button class="button-trash" id ="${item.id}" >
+    <button class="button-trash" id ="${item.id / 5}" >
       <img class="button-trash-image" src="img/Trash.svg">
       </button>
-    <button class="button-editing"  id = "${item.id}">
+    <button class="button-editing"  id = "${item.id / 6}">
       <img class="button-editing-image" src="${
         item.checked ? "" : "img/Pencil.svg"
       }">
@@ -138,11 +142,12 @@ function addClick() {
   });
   currentTasks.innerHTML = displayMessage;
   let trash = Array.from(document.querySelectorAll(`.button-trash`));
+
   trash.forEach((item, i) => {
     item.addEventListener("click", function (event) {
-      if (item.id == toDoList[i].id) {
+      if (item.id == toDoList[i].id / 5) {
         toDoList.splice(i, 1);
-        localStorage.setItem("todo", JSON.stringify(toDoList));
+        localStorage.setItem("list-todo", JSON.stringify(toDoList));
       }
       displayMessages();
     });
@@ -150,7 +155,7 @@ function addClick() {
 
   const labelText = Array.from(document.querySelectorAll(`.text-list`));
   const editing = Array.from(document.querySelectorAll(`.button-editing`));
-  const inputText = Array.from(document.querySelectorAll(`#editing`));
+  const inputText = Array.from(document.querySelectorAll(`#editing-input`));
   const editingImage = Array.from(
     document.querySelectorAll(`.button-editing-image`)
   );
@@ -161,15 +166,17 @@ function addClick() {
       editingImage[i].src = "./img/Done.svg";
     });
   });
+
   inputText.forEach((item, i) => {
     item.addEventListener("change", function (event) {
       toDoList[i].toDo = this.value;
-      localStorage.setItem("todo", JSON.stringify(toDoList));
+      localStorage.setItem("list-todo", JSON.stringify(toDoList));
       displayMessages();
     });
   });
 
   let elem = Array.from(document.querySelectorAll(`.todoli`));
+
   elem.forEach((item) => {
     item.addEventListener("change", function (event) {
       changeStatus(event);
@@ -186,14 +193,15 @@ function changeStatus(event) {
   let idInput = event.target.dataset.id;
 
   for (let i = 0; i < toDoList.length; i++) {
-    if (toDoList[i].id == idInput) {
+    if (toDoList[i].id / 2 == idInput) {
       toDoList[i].checked = !toDoList[i].checked;
       toDoList.sort(byField("checked"));
-      localStorage.setItem("todo", JSON.stringify(toDoList));
+      localStorage.setItem("list-todo", JSON.stringify(toDoList));
       break;
     }
   }
 }
+
 function handleDragStart(e) {
   this.style.opacity = "0.4";
 }
@@ -252,7 +260,7 @@ function handleDrop(e) {
       toDoList[itemIndex2],
       toDoList[itemIndex1],
     ];
-    localStorage.setItem("todo", JSON.stringify(toDoList));
+    localStorage.setItem("list-todo", JSON.stringify(toDoList));
     displayMessages();
   }
 
@@ -261,6 +269,7 @@ function handleDrop(e) {
 
 function dragAndDropItems() {
   let items = document.querySelectorAll(".wrapper .item");
+
   items.forEach(function (item) {
     item.addEventListener("dragstart", handleDragStart);
     item.addEventListener("dragover", handleDragOver);
